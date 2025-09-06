8f51e156b2a998153ac699fededa03cf2ca28816
Adalberto Junior <adalrsjr1@gmail.com>
2025-09-06 12:46:49 -0400

    add LSP settings for python

--- lsp.lua
+++ lsp.lua
@@ -7,9 +7,6 @@ local autocmd = vim.api.nvim_create_autocmd
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local opts = {buffer = args.buf}
-        -- open a new window line :help to show lsp commands
-        vim.keymap.set('n', '<leader>?', ':sv $MYVIMRC<cr>')
-
         vim.bo[args.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
         vim.keymap.set('i', '<c-space>', '<c-x><c-o>')
         vim.keymap.set('i', '.,', '<c-x><c-o>')
@@ -40,5 +37,46 @@ vim.api.nvim_create_autocmd('CompleteDone', {
 })
 
 
-vim.lsp.enable('pyright')
 vim.lsp.enable('gopls')
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright, otherwise auto-complete doesn't work
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
+
+require('lspconfig').pyright.setup {
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
+}
+vim.lsp.enable('pyright')
+
+vim.lsp.config('ruff', {
+    init_options = {
+        settings = {
+            -- Ruff language server settings go here
+        }
+    }
+})
+vim.lsp.enable('ruff')
+
